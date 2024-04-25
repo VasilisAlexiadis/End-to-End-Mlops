8d87cbf31e3080d9e9e4b02651e5a7b6603b26a7
VasilisAlexiadis <valeks4444@gmail.com>
2024-04-26 00:22:54 +0300

    next

--- mlruns/ML_REST.py
+++ mlruns/ML_REST.py
@@ -1,9 +1,16 @@
 import json
 import requests
 
+# The URL for the MLflow model serving endpoint
 url = 'http://localhost:5000/invocations'
 headers = {'Content-Type': 'application/json'}
 
-request_data = json.dumps({'columns': [["sepal_length", "sepal_width", "petal_length", "petal_width"],"data": [[6.3, 3.3, 6.0, 2.5]]}})
+# Prepare the request data as a JSON payload
+request_data = json.dumps({
+    'columns': ["sepal_length", "sepal_width", "petal_length", "petal_width"],
+    'data': [[6.3, 3.3, 6.0, 2.5]]
+})
+
+# Make the POST request and print the response
 response = requests.post(url, headers=headers, data=request_data)
 print(response.text)
